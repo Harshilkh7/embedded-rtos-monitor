36d2ff166b78e7b421bf7704f9041cddb00899e1
Harshil Khandelwal <161931283+Harshilkh7@users.noreply.github.com>
2026-09-08 12:45:41 +0530

    Add firmware telemetry interface

---
+++ firmware/inc/telemetry.h
@@ -0,0 +1,18 @@
+#ifndef TELEMETRY_H
+#define TELEMETRY_H
+
+#include <stdint.h>
+
+typedef struct {
+    uint16_t sequence;
+    uint16_t voltage_mv;
+    uint16_t current_ma;
+    int16_t temperature_centi_c;
+    uint8_t fault_code;
+} telemetry_sample_t;
+
+uint8_t telemetry_crc8(const uint8_t *data, uint16_t length);
+uint16_t telemetry_encode(const telemetry_sample_t *sample,
+                          uint8_t *frame, uint16_t capacity);
+
+#endif
