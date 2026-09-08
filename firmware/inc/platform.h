97c055e955f19f2e9b8d1ec20a546f572604abb0
Harshil Khandelwal <161931283+Harshilkh7@users.noreply.github.com>
2026-09-08 19:46:13 +0530

    Define ESP32 platform abstraction for sensors and watchdog

--- firmware/inc/platform.h
+++ firmware/inc/platform.h
@@ -6,7 +6,7 @@
 void platform_init(void);
 uint16_t adc_read_voltage_mv(void);
 uint16_t adc_read_current_ma(void);
-uint16_t adc_read_temperature_c(void);
+float adc_read_temperature_c(void);
 void uart_send(const uint8_t *data, uint16_t length);
 void watchdog_kick(void);
 
